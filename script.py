6fdcb92791d70a1cb2c464dcc66dd575897e3d3f
arodriguez2222 <arodriguez@ieee.org>
2022-08-09 16:16:26 -0500

    se actualiza el archivo

--- script.py
+++ script.py
@@ -5,4 +5,8 @@ from pathlib import Path
 base = Path.cwd()
 baseDir = base / 'data'
 file = baseDir / 'PSO20220223.xls'
-data = pd.read_excel()
+data = pd.read_excel(file,sheet_name=1)
+
+data2 = data.groupby(['linea','sentido','tipodia','trayecto']).agg({'tr_optimo':'min','tr_minimo':'min','tr_maximo':'max','nodo_1':'first','nodo_2':'last'})
+data2.rename(columns={'trayecto':'sublinea','nodo_2':'ultimo'},inplace=True)
+data2.to_csv('data/resultadoprueba.csv')
